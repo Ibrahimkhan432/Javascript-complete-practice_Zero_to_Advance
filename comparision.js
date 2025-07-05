2cdf8e2b5509cc1aec273bbfe99732c56463e23b
ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2025-07-05 23:54:14 +0500

    comparision

---
+++ comparision.js
@@ -0,0 +1,16 @@
+// console.log("10" > 1); //diff data types, string vs number
+
+// console.log(null > 0);
+
+// console.log(null >= 0); // true, null is considered equal to 0 in non-strict comparison
+
+// console.log(null < 0); // false, null is not less than 0 
+
+// console.log(null == 0); // true, non-strict comparison considers null equal to 0
+
+// console.log(null >= 0); // true, null is considered equal to 0 in non-strict comparison
+
+// console.log(null == 0); // false, null is not less than 0
+
+// console.log("10" === 10); // false, strict equality checks type and value
+
