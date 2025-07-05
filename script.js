e94003e142f6aa588cde9038623cbd3d8a732b39
ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2025-07-05 23:04:55 +0500

    data types

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+// console.log("hello world");
+// var a = "ibrahim" 
+// const b = "khan"
+// console.table([a,b]);  will print the values in a table format
+
+// console.log(typeof null) //object 
+// console.log(typeof undefined) //undefined
+
+// let name; // undefined
+// let age = 25; // number
