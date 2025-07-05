24f9f3e44ae8b70375e48b5dc427db15f61fe706
ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2025-07-06 03:05:42 +0500

    all about maths

---
+++ math.js
@@ -0,0 +1,15 @@
+// console.log(Math.random()); // Generates a random number between 0 (inclusive) and 1 (exclusive)
+
+// console.log(Math.round(2.33))
+
+// console.log(Math.floor(Math.random( 2.33))); // 2 lower
+
+// console.log(Math.floor(Math.ceil( 2.33))); // 3 upper
+
+// console.log(Math.ceil(Math.random() * 10)); // Generates a random integer between 1 and 100
+
+// // min and max range
+// const min = 2;
+// const max = 10;
+
+// console.log(Math.floor(Math.random() * (max - min + 1)+ min)); // Generates a random integer between 2 and 10
