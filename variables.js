2071005914dcac14360cac7de6c7b3f35a45443c
ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2025-07-05 23:44:03 +0500

    operator types with variables

---
+++ variables.js
@@ -0,0 +1,32 @@
+// let num = 10;
+// let string = String(num);
+
+// console.log(string); // "10"
+// console.log(typeof string); // "string"
+
+          // operations
+
+// let val = 10
+// let negativeVal = -val
+// console.log(negativeVal); // -10
+
+// console.log(2/2);
+// console.log(2%2); 
+
+// let str1 = "ibrahim";
+// let str2 = " khan";
+
+// console.log(str1+ str2); // "ibrahim khan"
+// log("")  -->false 0
+
+
+// console.log( 1 + 2 )
+// console.log( 1 + "2" )  //same
+// console.log( "1" + 2 )   //same
+// console.log( 1 + 2 + "3" ) // first adds 1 and 2, then concatenates "3"
+// console.log( "1" + 2 + 3 ) // first concatenates "1" and 2, then adds 3
+// console.log( 1 + "2" + 3 ) // first concatenates "1" and "2", then adds 3
+
+// see deatail in https://tc39.es/ecma262/
+
+
