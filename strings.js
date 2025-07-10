e0b6d5cc46a7447f477ac45bb48f5aae762a79aa
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-10 20:13:45 +0500

    string methods

---
+++ strings.js
@@ -0,0 +1,26 @@
+
+// const test = new String("ibrahim");
+
+// const substring = test.substring(0, 4)
+// console.log(substring)
+
+// const slice = test.slice(1, -3)
+// console.log(slice)
+
+
+// const names = "   trim  " 
+// console.log(names.trim())
+// console.log(names.trimStart())
+// console.log(names.trimEnd())
+
+// const value = "ibrahim=khan=splitted=value"
+// const replaced = value.replace("%","and")
+// console.log(replaced)
+
+
+// const finds = value.includes("i")
+// console.log(finds)  // true
+
+
+// const finds = value.split('=')
+// console.log(finds)  
