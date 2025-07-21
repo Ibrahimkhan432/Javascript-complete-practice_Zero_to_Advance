c6381dda2ee72ea5614d9e64d09d3bc410f7d88b
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-21 20:34:52 +0500

    mini calculator using nested condition

---
+++ ifControlFlow.js
@@ -0,0 +1,26 @@
+
+// var value = 20
+// if(value >10 ){
+// var names= "ibrahim"              
+//     console.log("this is " + names)
+// }
+// console.log("this is " + names) //let will give error
+
+
+    ///* calculator
+// var num1 = Number(prompt("enter number 1"));
+// var num2 = Number(prompt("enter number 2"));
+// var operator = prompt("enter operator")
+
+// if (operator== "+"){
+// console.log(num1 + num2)
+// }
+// else if (operator== "-"){
+// console.log(num1 - num2)
+// }
+// else if (operator== "*"){
+// console.log(num1 * num2)
+// }
+// else{
+//     console.log("you gave nothing")
+// }
